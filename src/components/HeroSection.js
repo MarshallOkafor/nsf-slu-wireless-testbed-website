edda1d0ac89d01074a35f454d829697a61fbdccf
Marshall Okafor <marshallokafor@gmail.com>
2024-06-18 11:15:05 -0500

    more bug fixes

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <img src="/hero-2.jpg" alt="" />
+      <img src="/nsf-slu-wireless-testbed-website/hero-2.jpg" alt="" />
       <h1>SOFTWARE-DEFINED EDGE INFRASTRUCTURE</h1>
       <p>Full-stack Data-Driven Wireless Network Application</p>
     </div>
