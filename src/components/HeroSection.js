ef8772f6a692ddfb55687b7eee23b3c3180d5fb0
Marshall Okafor <marshallokafor@gmail.com>
2024-06-17 21:51:00 -0500

    changed video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src="/videos/data-center-video.mp4" autoPlay loop muted />
+      <video src="/videos/dc-2.mp4" autoPlay loop muted />
       <h1>SOFTWARE-DEFINED EDGE INFRASTRUCTURE</h1>
       <p>Full-stack Data-Driven Wireless Network Application</p>
     </div>
