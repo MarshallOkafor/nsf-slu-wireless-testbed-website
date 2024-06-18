259f9cfaf571eeadfbf5192156edc541da9dd95d
Marshall Okafor <marshallokafor@gmail.com>
2024-06-18 12:24:05 -0500

    bugs

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <img src={`${process.env.PUBLIC_URL}/hero-2.jpg`} alt="" />
+      <img src={`${process.env.PUBLIC_URL}/giphy.gif`} alt="" />
       <h1>SOFTWARE-DEFINED EDGE INFRASTRUCTURE</h1>
       <p>Full-stack Data-Driven Wireless Network Application</p>
     </div>
