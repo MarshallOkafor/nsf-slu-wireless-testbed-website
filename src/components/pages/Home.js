df3b9c8799865c5981b0b996cad65b6350a2d360
Marshall Okafor <marshallokafor@gmail.com>
2024-07-08 11:31:14 -0500

    Revert "Added Main Section with resolution specs"
    This reverts commit d7ceaf45f1760cb3c2b970e79b500eaa5e055deb.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,10 @@
 import '../../App.css';
 import HeroSection from '../HeroSection';
-import MainSection from '../MainSection';
 
 function Home() {
   return (
     <>
       <HeroSection />
-      <MainSection />
     </>
   );
 }
