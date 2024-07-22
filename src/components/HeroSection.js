dcae6d1b3efb4629bf83ce83bd637c8c60408c13
ZulqerAbduwasit <zukai.sagan@gmail.com>
2024-07-22 10:39:36 -0500

    Refined Alt Text

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <img src={`${process.env.PUBLIC_URL}/hero-2.jpg`} alt="Server Room" />
+      <img src={`${process.env.PUBLIC_URL}/hero-2.jpg`} alt="Dimly lit server room with rows of black server racks illuminated by blue lights." />
       <h1>SOFTWARE-DEFINED EDGE INFRASTRUCTURE</h1>
       <p>Full-stack Data-Driven Wireless Network Application</p>
     </div>
