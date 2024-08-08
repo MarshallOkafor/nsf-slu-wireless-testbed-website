ef7016df65631f70810f769036653b462597eda0
ZulqerAbduwasit <zukai.sagan@gmail.com>
2024-08-08 10:47:45 -0500

    Various css adjustments

--- src/components/PublicationsMain.js
+++ src/components/PublicationsMain.js
@@ -11,63 +11,100 @@ function PublicationsMain() {
 
 <> <div className='publications-main-container'>
       <p className="firstp"><span>(Best student paper award)</span> 
-      <br></br> Pinto, Andrea and Santaromita, Giuseppe and Fiandrino, Claudio and Giustiniano, Domenico and Esposito, Flavio,
-       <br></br>"Characterizing Location Management Function Performance in 5G Core Networks",
-        <br></br><span> 2022 IEEE Conference on Network Function Virtualization and Software Defined Networks (NFV-SDN)
+      <br /> Pinto, Andrea and Santaromita, Giuseppe and Fiandrino, Claudio and Giustiniano, Domenico and Esposito, Flavio,
+       <br />"Characterizing Location Management Function Performance in 5G Core Networks",
+        <br /><span> 2022 IEEE Conference on Network Function Virtualization and Software Defined Networks (NFV-SDN)
         </span>
-        , 2022. <br></br>[
+        , 2022. <br />[
           <a href="https://ieeexplore.ieee.org/document/9974927" target="_blank" rel="noopener noreferrer">paper</a>
           ] [
             <button onClick={()=>setFirstButtonPopup(true)}>bibtex</button>] [
               <a href="https://github.com/pintauroo/5G_LMF" target="_blank" rel="noopener noreferrer">code</a>]
-              <Popup trigger={firstButtonPopup} setTrigger={setFirstButtonPopup}>
-              <h3>Bibtex</h3>
-              <p>Here is my bibtex</p>
-              </Popup>
-              </p>
               
+              </p>
+              <Popup trigger={firstButtonPopup} setTrigger={setFirstButtonPopup}>
+              <h3>Characterizing Location Management Function Performance in 5G Core Networks</h3>
+              <p>@INPROCEEDINGS&#123;9974927,
+                <br />&emsp;&emsp;author=&#123;Pinto, Andrea and Santaromita, Giuseppe and Fiandrino, Claudio and Giustiniano, Domenico and Esposito, Flavio&#125;,
+                <br />&emsp;&emsp;booktitle=&#123;2022 IEEE Conference on Network Function Virtualization and Software Defined Networks (NFV-SDN)&#125;, 
+  <br />&emsp;&emsp;title=&#123;Characterizing Location Management Function Performance in 5G Core Networks&#125;, 
+  <br />&emsp;&emsp;year=&#123;2022&#125;,
+  <br />&emsp;&emsp;volume=&#123;&#125;,
+  <br />&emsp;&emsp;number=&#123;&#125;,
+  <br />&emsp;&emsp;pages=&#123;66-71&#125;,
+  <br />&emsp;&emsp;doi=&#123;10.1109/NFV-SDN56302.2022.9974927&#125;
+  <br />&#125;</p>
+              </Popup>
        <p className="secondp">S. S. Bhavanasi, L. Pappone, and F. Esposito,
-        <br></br>"Dealing with Changes: Resilient Routing via Graph Neural Networks and Multi-Agent Deep Reinforcement Learning",
-        <br></br><span>  IEEE Transactions on Network and Service Management
+        <br />"Dealing with Changes: Resilient Routing via Graph Neural Networks and Multi-Agent Deep Reinforcement Learning",
+        <br /><span>  IEEE Transactions on Network and Service Management
         </span>
-        , 2023. <br></br>[
+        , 2023. <br />[
           <a href="https://ieeexplore.ieee.org/document/10158424" target="_blank" rel="noopener noreferrer">paper</a>
           ] [
             <button onClick={()=>setSecondButtonPopup(true)}>bibtex</button>]
+              
+              </p>  
               <Popup trigger={secondButtonPopup} setTrigger={setSecondButtonPopup}>
-              <h3>Bibtex</h3>
-              <p>Here is my bibtex</p>
-              </Popup>
-              </p>    
+              <h3>Dealing with Changes: Resilient Routing via Graph Neural Networks and Multi-Agent Deep Reinforcement Learning</h3>
+              <p>@ARTICLE&#123;10158424,
+  <br />&emsp;&emsp;author=&#123;Bhavanasi, Sai Shreyas and Pappone, Lorenzo and Esposito, Flavio&#125;,
+  <br />&emsp;&emsp;journal=&#123;IEEE Transactions on Network and Service Management&#125;,
+  <br />&emsp;&emsp;title=&#123;Dealing with Changes: Resilient Routing via Graph Neural Networks and Multi-Agent Deep Reinforcement Learning&#125;,
+  <br />&emsp;&emsp;year=&#123;2023&#125;,
+  <br />&emsp;&emsp;volume=&#123;&#125;,
+  <br />&emsp;&emsp;number=&#123;&#125;,
+  <br />&emsp;&emsp;pages=&#123;1-1&#125;,
+  <br />&emsp;&emsp;doi=&#123;10.1109/TNSM.2023.3287936&#125;&#125;
+  <br />&#125;
+</p>
+              </Popup>  
 
         <p className="thirdp">C. Puligheddu, J. Ashdown, C. F. Chiasserini, and F. Restuccia,
-        <br></br>"SEM-O-RAN: Semantic and Flexible O-RAN Slicing for NextG Edge-Assisted Mobile Systems”,
-        <br></br><span>  Proc. of IEEE  Conference on Computer Communications (IEEE INFOCOM)
+        <br />"SEM-O-RAN: Semantic and Flexible O-RAN Slicing for NextG Edge-Assisted Mobile Systems”,
+        <br /><span>  Proc. of IEEE  Conference on Computer Communications (IEEE INFOCOM)
         </span>
-        , Hoboken, New Jersey, United States, May 2023.  <br></br>[
+        , Hoboken, New Jersey, United States, May 2023.  <br />[
           <a href="https://arxiv.org/abs/2212.11853" target="_blank" rel="noopener noreferrer">paper</a>
           ] [
             <button onClick={()=>setThirdButtonPopup(true)}>bibtex</button>]
+              </p>  
               <Popup trigger={thirdButtonPopup} setTrigger={setThirdButtonPopup}>
-              <h3>Bibtex</h3>
-              <p>Here is my bibtex</p>
-              </Popup>
-              </p>   
+              <h3>SEM-O-RAN: Semantic and Flexible O-RAN Slicing for NextG Edge-Assisted Mobile Systems</h3>
+              <p>@misc&#123;puligheddu2022semoran,
+        <br />&emsp;&emsp;title=&#123;SEM-O-RAN: Semantic and Flexible O-RAN Slicing for NextG Edge-Assisted Mobile Systems&#125;,
+        <br />&emsp;&emsp;author=&#123;Puligheddu, Corrado and Ashdown, Jonathan and Chiasserini, Carla Fabiana and Restuccia, Francesco&#125;,
+        <br />&emsp;&emsp;year=&#123;2022&#125;,
+        <br />&emsp;&emsp;eprint=&#123;2212.11853&#125;,
+        <br />&emsp;&emsp;archivePrefix=&#123;arXiv&#125;,
+        <br />&emsp;&emsp;primaryClass=&#123;cs.NI&#125;
+        <br />&#125;</p>
+              </Popup> 
 
 
         <p className="fourthp">S. S. Bhavanasi, L. Pappone and F. Esposito,
-        <br></br>"Routing with Graph Convolutional Networks and Multi-Agent Deep Reinforcement Learning",
-        <br></br><span>  2022 IEEE Conference on Network Function Virtualization and Software Defined Networks (NFV-SDN)
+        <br />"Routing with Graph Convolutional Networks and Multi-Agent Deep Reinforcement Learning",
+        <br /><span>  2022 IEEE Conference on Network Function Virtualization and Software Defined Networks (NFV-SDN)
         </span>
-        , 2022.  <br></br>[
+        , 2022.  <br />[
           <a href="https://ieeexplore.ieee.org/document/9974607" target="_blank" rel="noopener noreferrer">paper</a>
           ] [
             <button onClick={()=>setFourthButtonPopup(true)}>bibtex</button>]
-              <Popup trigger={fourthButtonPopup} setTrigger={setFourthButtonPopup}>
-              <h3>Bibtex</h3>
-              <p>Here is my bibtex</p>
-              </Popup>
+              
               </p>     
+              <Popup trigger={fourthButtonPopup} setTrigger={setFourthButtonPopup}>
+              <h3>Routing with Graph Convolutional Networks and Multi-Agent Deep Reinforcement Learning</h3>
+              <p>@INPROCEEDINGS&#123;9974607,
+                <br />&emsp;&emsp;author=&#123;Bhavanasi, Sai Shreyas and Pappone, Lorenzo and Esposito, Flavio&#125;,
+                <br />&emsp;&emsp;booktitle=&#123;2022 IEEE Conference on Network Function Virtualization and Software Defined Networks (NFV-SDN)&#125;, 
+  <br />&emsp;&emsp;title=&#123;Routing with Graph Convolutional Networks and Multi-Agent Deep Reinforcement Learning&#125;, 
+  <br />&emsp;&emsp;year=&#123;2022&#125;,
+  <br />&emsp;&emsp;volume=&#123;&#125;,
+  <br />&emsp;&emsp;number=&#123;&#125;,
+  <br />&emsp;&emsp;pages=&#123;72-77&#125;,
+  <br />&emsp;&emsp;doi=&#123;10.1109/NFV-SDN56302.2022.9974607&#125;
+  <br />&#125;</p>
+              </Popup>
     </div>
     </>
   );
--- src/components/pages/Popup.js
+++ src/components/pages/Popup.js
@@ -4,7 +4,7 @@ import './Popup.css';
 function Popup(props) {
   return (props.trigger) ? (
     <div className="Popup">
-        <div>
+        <div className="popup-inner">
             <button className="close-btn" onClick={()=>props.setTrigger(false)}>&#x2715; 
             &#x00d7; </button>
             {props.children}
